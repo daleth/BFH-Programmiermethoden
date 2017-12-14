4320ac6a2d81e969b08021aafef42994c789a59f
daleth <daleth@users.noreply.github.com>
2017-12-14 13:30:13 +0100

    removed unused imports

--- SceneBuilder/src/ch/bfh/nowad1/dataEditor/DataEditorController.java
+++ SceneBuilder/src/ch/bfh/nowad1/dataEditor/DataEditorController.java
@@ -1,7 +1,5 @@
 package ch.bfh.nowad1.dataEditor;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
